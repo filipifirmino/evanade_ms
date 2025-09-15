c9303a9efb4ed666570604321202145d3bd81665
Filipi Firmino <contatoic3web@gmail.com>
2025-09-15 16:57:59 -0300

    update configure infra

--- Inventory/Inventory/Inventory.InfraStructure/Configure/DataContext.cs
+++ Inventory/Inventory/Inventory.InfraStructure/Configure/DataContext.cs
@@ -1,4 +1,4 @@
-﻿using Inventory.InfraStructure.Entitys;
+﻿using Inventory.InfraStructure.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
---
+++ Inventory/Inventory/Inventory.Web/Mappers/ProductMapper.cs
@@ -0,0 +1,11 @@
+﻿using Inventory.Application.Entities;
+using Inventory.Web.RequestsDto;
+
+namespace Inventory.Application.Mappers;
+
+public static class ProductMapper
+{
+    public static Product ToProduct(this ProductRequest product)
+        => new Product( product.Name, product.Description, product.Price, product.StockQuantity);
+
+}
