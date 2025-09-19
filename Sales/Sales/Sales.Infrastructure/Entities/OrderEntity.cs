b15696d818406babdaeb1113ef7e0912c4b2ea42
Filipi Firmino <contatoic3web@gmail.com>
2025-09-19 19:33:03 -0300

    update config context

--- Sales/Sales/Sales.Infrastructure/Entities/OrderEntity.cs
+++ Sales/Sales/Sales.Infrastructure/Entities/OrderEntity.cs
@@ -7,7 +7,7 @@ public class OrderEntity
 {
     public Guid OrderId { get; set; }
     public Guid CustomerId { get; set; }
-   public List<OrderItemEntity> Items { get; set; } = new();
+    public List<OrderItemEntity> Items { get; set; } = new();
     public decimal TotalAmount { get; set; }
     public  StatusEntity Status { get; set; }
 }
